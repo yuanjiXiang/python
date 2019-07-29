e30cfa80fdbd3b2e4496dac09513e1fc533bf75c
yuanjiXiang <46594308+yuanjiXiang@users.noreply.github.com>
2019-07-29 11:39:34 +0800

    Add files via upload

--- file_manage/file_manage.py
+++ file_manage/file_manage.py
@@ -3,7 +3,9 @@ import os, shutil, sys
 
 included_files = []
 
+
 def read_file_dictionary(file_path):
+    """读取 file_path 下所有最底层文件的文件路径"""
     if not os.path.exists(file_path):
         print(file_path + ' is wrong')
         return False
@@ -20,33 +22,37 @@ def read_file_dictionary(file_path):
 
 
 def judge_file_exit(src_path, des_path):
+    """判断源路径和目的路径是否存在"""
     if not os.path.exists(src_path):
         print(src_path + ' is wrong')
         return False
     else:
-        if not os.path.exists(des_path):
+        if not os.path.exists(des_path):  # 目的路径不存在则创建
             os.makedirs(des_path)
         return True
 
-def move_file(src_path, des_path):
-    if judge_file_exit(src_path, des_path):
-        shutil.move(src_path, des_path)
 
 def move_files(src_path, des_path):
+    """移动文件"""
     src_path_list = read_file_dictionary(src_path)
     for file_name in src_path_list:
         if judge_file_exit(file_name, des_path):
             shutil.move(file_name, des_path)
 
+
 def copy_file(src_path, des_path):
-    if judge_file_exit(src_path, des_path):
-        shutil.copy(src_path, des_path)
+    """复制文件"""
+    src_path_list = read_file_dictionary(src_path)
+    for file_name in src_path_list:
+        if judge_file_exit(file_name, des_path):
+            shutil.copy(file_name, des_path)
+
 
 def change_file_suffix(src_path_list, old_suffix, new_suffix):
+    """修改文件后缀名"""
     for file_name in src_path_list:
         if file_name.find(old_suffix) != -1:
             new_file_name = file_name.replace(old_suffix, new_suffix)
             os.rename(file_name, new_file_name)
 
 # src_path, des_path = sys.argv[1], sys.argv[2]
-
--- file_manage/test.py
+++ file_manage/test.py
@@ -1,5 +1,9 @@
 import file_manage
 
-src_path = '/Users/xiang/Downloads/predictPics4'
-file_manage.change_file_suffix(file_manage.read_file_dictionary(src_path), '.bmp', '.png')
-print('done')
+src_path = '/Users/xiang/kaggle/data/train'
+des_path = '/Users/xiang/kaggle/data/preprocessing_train'
+
+# file_manage.copy_file(src_path, des_path)
+print(len(file_manage.read_file_dictionary(src_path)))
+# print(file_manage.read_file_dictionary(src_path))
+print(len(file_manage.read_file_dictionary(des_path)))
