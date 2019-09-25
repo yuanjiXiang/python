bfc145c68805d54f3ababd6ef988f141c5744f81
yuanjiXiang <46594308+yuanjiXiang@users.noreply.github.com>
2019-09-25 18:23:37 +0800

    Create dict_sort.py

---
+++ 2/dict_sort.py
@@ -0,0 +1,20 @@
+# 字典排序
+
+# 排序字典的键
+def dictionairy1():  
+  
+    # 声明字典
+    key_value ={}     
+ 
+    # 初始化
+    key_value[2] = 56       
+    key_value[1] = 2 
+    key_value[5] = 12 
+    key_value[4] = 24
+    key_value[6] = 18      
+    key_value[3] = 323 
+
+    # for i in sorted(key_value):
+    #     print(i)
+    print(sorted(key_value))
+dictionairy1()
