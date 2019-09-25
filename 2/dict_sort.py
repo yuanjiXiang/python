b1191cfef1fd43898f15ef3bd947a6996b402d7b
yuanjiXiang <46594308+yuanjiXiang@users.noreply.github.com>
2019-09-25 21:22:39 +0800

    Update dict_sort.py

--- 2/dict_sort.py
+++ 2/dict_sort.py
@@ -1,20 +1,50 @@
 # 字典排序
 
-# 排序字典的键
-def dictionairy1():  
-  
-    # 声明字典
-    key_value ={}     
+key_value ={}     
  
-    # 初始化
-    key_value[2] = 56       
-    key_value[1] = 2 
-    key_value[5] = 12 
-    key_value[4] = 24
-    key_value[6] = 18      
-    key_value[3] = 323 
+# 初始化
+key_value[1] = 2 
+key_value[2] = 56       
+key_value[3] = 323 
+key_value[4] = 24
+key_value[5] = 24 
+key_value[6] = 18      
 
-    # for i in sorted(key_value):
+
+# 按键排序
+def dictionairy1(dict):  
+    # for i in sorted(dict):
     #     print(i)
-    print(sorted(key_value))
-dictionairy1()
+    print('按键排序')
+    print(sorted(dict.items(), key = lambda di:di[0]))
+
+dictionairy1(key_value)
+
+# 按值排序   
+def dictionairy2(dict):
+    print('按值排序')
+    print(sorted(dict.items(), key = lambda di:di[1]))
+
+dictionairy2(key_value)
+
+# 字典列表排序
+lis = [{ "name" : "Taobao", "age" : 100},  
+{ "name" : "Runoob", "age" : 7 }, 
+{ "name" : "Google", "age" : 100 }, 
+{ "name" : "Wiki" , "age" : 200 }] 
+  
+# 通过 age 升序排序
+print ("列表通过 age 升序排序: ")
+print (sorted(lis, key = lambda i: i['age']) )
+  
+print ("\r") 
+  
+# 先按 age 排序，再按 name 排序
+print ("列表通过 age 和 name 排序: ")
+print (sorted(lis, key = lambda i: (i['age'], i['name'])) )
+  
+print ("\r") 
+  
+# 按 age 降序排序
+print ("列表通过 age 降序排序: ")
+print (sorted(lis, key = lambda i: i['age'],reverse=True) )
