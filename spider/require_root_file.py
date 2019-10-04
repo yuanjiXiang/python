f59dc15059c882c678378c5c8e8666c1165f1590
yuanjiXiang <739203572@qq.com>
2019-10-04 20:57:02 +0800

    .

--- spider/require_root_file.py
+++ spider/require_root_file.py
@@ -1,6 +1,9 @@
 import urllib.request
+import ssl
 
 website = 'https://www.huya.com/g/lol'
 
-web_info = urllib.request.urlopen(website)  # 获取原始网页
+context = ssl._create_unverified_context()
+
+web_info = urllib.request.urlopen(website, context=context)  # 获取原始网页
 web_info = web_info.read().decode('utf-8')  # 对原网页进行解码
